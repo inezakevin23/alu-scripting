926d652d43164b9fb9c2a21511138b5268c90340
ineza kevin <k.ineza1@alustudent.com>
2025-10-26 09:26:55 +0000

    updated script

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -13,7 +13,7 @@ def top_ten(subreddit):
     If the subreddit is invalid, prints None.
     """
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent":  "ALUStudent/1.0"}
     params = {"limit": 10}
 
     response = requests.get(url, headers=headers, params=params,
