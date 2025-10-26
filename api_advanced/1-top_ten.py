0feff72cc009f27af60b0f88587b350a0f588280
ineza kevin <k.ineza1@alustudent.com>
2025-10-26 09:33:33 +0000

    updated script

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and prints the titles of the first 10
+prints the titles of the first 10
 hot posts listed for a given subreddit.
 """
 
@@ -9,8 +9,7 @@ import requests
 
 def top_ten(subreddit):
     """
-    Prints the titles of the first 10 hot posts for a given subreddit.
-    If the subreddit is invalid, prints None.
+    Prints the titles of the first 10 hot posts.
     """
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {"User-Agent":  "ALUStudent/1.0"}
@@ -20,12 +19,12 @@ def top_ten(subreddit):
                             allow_redirects=False)
 
     if response.status_code != 200:
-        print("ok",end="")
+        print("OK",end="")
         return
 
     data = response.json().get("data")
     if not data or "children" not in data:
-        print("ok",end="")
+        print("OK",end="")
         return
 
     for post in data.get("children", []):
