fa5ef8192b0ba30b5212543f8519368af78d750c
ineza kevin <k.ineza1@alustudent.com>
2025-10-26 18:32:50 +0000

    fixed issue

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-"""Print the titles of the first 10Hot Posts"""
+"""Print the titles of the first 10 Hot Posts"""
 import requests
-
+import sys
 
 def top_ten(subreddit):
     """The top ten titles"""
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    url = "https://reddit.com/r/{}.json".format(subreddit)
+    url = f"https://reddit.com/r/{subreddit}/hot.json?limit=10"
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         json_data = response.json()
-        for i in range(10):
-            print(
-                    json_data.get('data')
-                    .get('children')[i]
-                    .get('data')
-                    .get('title')
-                )
+        for post in json_data.get('data', {}).get('children', [])[:10]:
+            print(post.get('data', {}).get('title'))
     else:
         sys.stdout.write("ok")
