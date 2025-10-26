934cc8588e5937300aaab629523b8796318e2388
ineza kevin <k.ineza1@alustudent.com>
2025-10-26 09:15:01 +0000

    updated script

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,23 +1,34 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
+"""
+Queries the Reddit API and prints the titles of the first 10
+hot posts listed for a given subreddit.
+"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
+    """
+    Prints the titles of the first 10 hot posts for a given subreddit.
+    If the subreddit is invalid, prints None.
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    params = {"limit": 10}
 
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}/hot.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
 
-    if response.status_code == 200:
-        json_data = response.json()
-        posts = json_data.get('data', {}).get('children', [])
-        if posts:
-            for i in range(min(10, len(posts))):
-                print(posts[i].get('data', {}).get('title'))
-        else:
-            print("OK")
-    else:
-        print("OK")
+    if response.status_code != 200:
+        print(None)
+        return
+
+    data = response.json().get("data")
+    if not data or "children" not in data:
+        print(None)
+        return
+
+    for post in data.get("children", []):
+        title = post.get("data", {}).get("title")
+        if title:
+            print(title)
