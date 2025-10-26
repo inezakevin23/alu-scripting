d1cc4c163b42bdf05ca247d89979b446deecc30e
ineza kevin <k.ineza1@alustudent.com>
2025-10-26 10:50:32 +0000

    fixed issue

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
-"""Module that queries the Reddit API and prints the titlesof the first 10 hot posts for a given subreddit."""
+"""Print the titles of the first 10Hot Posts"""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts for a subreddit.
+    """The top ten titles"""
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(url, headers=headers)
 
-    Args:
-        subreddit (str) : the subreddit to query 
-
-    Returns:
-        string : prints the titles of the first 10 hot posts listed for a given subreddit. 
-    
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "MyRedditApp/0.1"}
-    params = {"limit": 100}
-
-    try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-
-        # If subreddit is invalid, Reddit returns 302 or 404
-        if response.status_code != 200:
-            print(None)
-            return
-
-        data = response.json()
-        for child in data["data"]["children"]:
-            print(child["data"]["title"])
-
-    except Exception:
+    if response.status_code == 200:
+        json_data = response.json()
+        for i in range(10):
+            print(
+                    json_data.get('data')
+                    .get('children')[i]
+                    .get('data')
+                    .get('title')
+                )
+    else:
         print(None)
